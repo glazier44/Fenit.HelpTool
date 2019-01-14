44165a9a97a334f5d16210ddb5b1efa3da54d186
Tomek Kwiek <kwiek_tomasz@hotmail.com>
2019-01-14 19:44:31 +0100

    save

---
+++ Fenit.HelpTool.Core.SqlLite/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fenit.HelpTool.Core.SqlLite
+{
+    class Class1
+    {
+    }
+}
