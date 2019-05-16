7e6c19ab8cb0512f44dd92c049a34f4769ca02c7
Tomek Kwiek <kwiek_tomasz@hotmail.com>
2019-05-16 21:40:08 +0200

    Start

---
+++ Fenit.HelpTool.Module.Settings/ShifterModule.cs
@@ -0,0 +1,30 @@
+﻿using Fenit.HelpTool.Module.Shifter.ViewModels;
+using Fenit.HelpTool.Module.Shifter.Views;
+using Fenit.HelpTool.UI.Core;
+using Fenit.HelpTool.UI.Core.Base;
+using Prism.Ioc;
+using Prism.Regions;
+using Unity;
+
+namespace Fenit.HelpTool.Module.Shifter
+{
+    public class ShifterModule : BaseModule
+    {
+        public ShifterModule(IUnityContainer container) : base(container)
+        {
+        }
+
+
+        public override void OnInitializedModule(IContainerProvider containerProvider)
+        {
+            var regionManager = containerProvider.Resolve<IRegionManager>();
+            regionManager.Initialize(containerProvider.Resolve<MainWindow>(), ViewReservoir.ShifterModule.Main);
+        }
+
+        public override void RegisterModuleTypes(IContainerRegistry containerRegistry)
+        {
+            containerRegistry.RegisterForNavigation<MainWindow>();
+            RegisterDialog<MessageWindow, MessageViewModel>(ViewReservoir.ShifterModule.MessageWindow);
+        }
+    }
+}
---
+++ Fenit.HelpTool.Module.Settings/Views/MessageWindow.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace Fenit.HelpTool.Module.Shifter.Views
+{
+    /// <summary>
+    ///     Interaction logic for Message.xaml
+    /// </summary>
+    public partial class MessageWindow
+    {
+        public MessageWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
