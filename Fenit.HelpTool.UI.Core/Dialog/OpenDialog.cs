78932a900ac9557d1ee47f3677d0fa7e37ca84d4
Tomek Kwiek <kwiek_tomasz@hotmail.com>
2019-09-20 23:09:36 +0200

    Update nuget

--- Fenit.HelpTool.UI.Core/Dialog/OpenDialog.cs
+++ Fenit.HelpTool.UI.Core/Dialog/OpenDialog.cs
@@ -1,31 +1,30 @@
-﻿using Fenit.Toolbox.Core.Answers;
-using Gat.Controls;
+﻿using System.IO;
+using Fenit.Toolbox.Core.Answers;
+using Microsoft.WindowsAPICodePack.Dialogs;
 
 namespace Fenit.HelpTool.UI.Core.Dialog
 {
     public class OpenDialog
     {
-        private readonly OpenDialogViewModel _viewModel;
-
-        public OpenDialog()
-        {
-            var openDialog = new OpenDialogView();
-            _viewModel = (OpenDialogViewModel) openDialog.DataContext;
-        }
-
-
         public Response<string> SelectFolder()
         {
             var res = new Response<string>();
 
-            _viewModel.SelectFolder = true;
-            _viewModel.IsDirectoryChooser = true;
 
-            var resDialog = _viewModel.Show();
-            if (resDialog.HasValue && resDialog.Value && _viewModel.SelectFolder)
-                res.AddValue(_viewModel.SelectedFilePath);
+            var dialog = new CommonOpenFileDialog {IsFolderPicker = true};
+            var result = dialog.ShowDialog();
+
+            if (result == CommonFileDialogResult.Ok)
+            {
+                var dirToProcess = Directory.Exists(dialog.FileName)
+                    ? dialog.FileName
+                    : Path.GetDirectoryName(dialog.FileName);
+                res.AddValue(dirToProcess);
+            }
             else
-                res.AddError("nie wybrano");
+            {
+                res.AddError("No folder selected");
+            }
 
             return res;
         }
