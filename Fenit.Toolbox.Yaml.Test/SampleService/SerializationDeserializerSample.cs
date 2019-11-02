2d9958ea5483bcbea1a16fd7b0c9115cfb2484ac
Tomek Kwiek <kwiek_tomasz@hotmail.com>
2019-11-02 21:14:33 +0100

    Add yamltest

---
+++ Fenit.Toolbox.Yaml.Test/SampleService/SerializationDeserializerSample.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Fenit.Toolbox.Yaml.Test.Models;
+using YamlDotNet.Serialization;
+
+namespace Fenit.Toolbox.Yaml.Test.SampleService
+{
+    public class SerializationDeserializerSample
+    {
+
+        public void First()
+        {
+            var deserializer = new YamlDotNet.Serialization.Deserializer();
+            var dict = deserializer.Deserialize<Dictionary<string, string>>("hello: world");
+            Console.WriteLine(dict["hello"]);
+        }
+
+        public void ClassDes()
+        {
+            var yamlInput = 
+@"- Name: Oz-Ware
+  PhoneNumber: 123456789";
+            var deserializer = new DeserializerBuilder()
+                .Build();
+
+            var contacts = deserializer.Deserialize<List<Contact>>(yamlInput);
+            Console.WriteLine(contacts[0]);
+        }
+    }
+}
